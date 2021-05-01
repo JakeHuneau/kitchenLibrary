0342cb2c925934d702a2bea5d00c15b8b794cf40
JakeHuneau <jakehuneau@yahoo.com>
2021-05-01 15:20:52 -0400

    Added cookies and disbale buttons during API calls

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import KitchenLibrary from './KitchenLibrary';
-import { CookiesProvider } from 'react-cookie';
 
 ReactDOM.render(
   <React.StrictMode>
-      <CookiesProvider>
     <KitchenLibrary />
-      </CookiesProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
